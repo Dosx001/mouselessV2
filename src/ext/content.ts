a4fc3ec4f353c71ada5b200da2a2776fbc4cf898
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2022-12-19 10:06:58 -0800

    Ignored elements at (0,0) but not (0,y) and (x,0)

--- src/ext/content.ts
+++ src/ext/content.ts
@@ -207,9 +207,10 @@ const blobList = {
       )
         continue;
       const rect = linkElem.getBoundingClientRect();
+      if (rect.top === 0 && rect.left === 0) continue;
       if (
-        0 < rect.top &&
-        0 < rect.left &&
+        0 <= rect.top &&
+        0 <= rect.left &&
         rect.bottom <=
         (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <=

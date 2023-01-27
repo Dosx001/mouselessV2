7dec9613976e2d39529a96c7e2f859ee73534501
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-01-26 20:05:50 -0800

    Clean up background

--- src/ext/background.ts
+++ src/ext/background.ts
@@ -1,17 +1,12 @@
-const getIndex = async (off: number) => {
-  const tab = (
-    await browser.tabs.query({ active: true, currentWindow: true })
-  )[0];
+const getIndex = async (sender: browser.runtime.MessageSender, off: number) => {
   const tabCount = (await browser.tabs.query({ currentWindow: true })).length;
-  let idx = tab.index + off;
-  if (idx < 0) idx = tabCount - 1;
-  else if (idx >= tabCount) idx = 0;
-  return idx;
+  const idx = sender.tab!.index + off;
+  return idx === -1 ? tabCount - 1 : tabCount === idx ? 0 : idx;
 };
 
 const file = { file: "ext/styles.css" };
-browser.tabs.query({}).then((res) => {
-  for (const tab of res) browser.tabs.insertCSS(tab.id!, file);
+browser.tabs.query({}).then((tabs) => {
+  for (const tab of tabs) browser.tabs.insertCSS(tab.id!, file);
 });
 
 browser.tabs.onUpdated.addListener((_, info) => {
@@ -24,31 +19,25 @@ browser.runtime.onMessage.addListener(async (msg, sender) => {
       browser.tabs.insertCSS(file);
       break;
     case "changeTabLeft": {
-      const idx = await getIndex(-1);
-      browser.tabs.update(
-        (await browser.tabs.query({ currentWindow: true, index: idx }))[0].id!,
-        { active: true }
-      );
+      const query = { currentWindow: true, index: await getIndex(sender, -1) };
+      browser.tabs.update((await browser.tabs.query(query))[0].id!, {
+        active: true,
+      });
       break;
     }
     case "changeTabRight": {
-      const idx = await getIndex(1);
-      browser.tabs.update(
-        (await browser.tabs.query({ currentWindow: true, index: idx }))[0].id!,
-        { active: true }
-      );
+      const query = { currentWindow: true, index: await getIndex(sender, 1) };
+      browser.tabs.update((await browser.tabs.query(query))[0].id!, {
+        active: true,
+      });
       break;
     }
-    case "moveTabLeft": {
-      const idx = await getIndex(-1);
-      browser.tabs.move(sender.tab!.id!, { index: idx });
+    case "moveTabLeft":
+      browser.tabs.move(sender.tab!.id!, { index: await getIndex(sender, -1) });
       break;
-    }
-    case "moveTabRight": {
-      const idx = await getIndex(1);
-      browser.tabs.move(sender.tab!.id!, { index: idx });
+    case "moveTabRight":
+      browser.tabs.move(sender.tab!.id!, { index: await getIndex(sender, 1) });
       break;
-    }
     case "openTab":
       browser.tabs.create({ url: msg.href });
       break;

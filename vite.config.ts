405fad7e465e0d1d235f135f6a5d5883a1211b63
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-08-22 10:24:37 -0700

    ci(vite): convert config to typescript

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    target: "esnext",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        content: "./src/ext/content.ts",
+        background: "./src/ext/background.ts",
+        popup: "./src/popup/index.html",
+        options: "./src/settings/options.html",
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name][extname]",
+      },
+    },
+  },
+});

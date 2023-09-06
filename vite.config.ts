f8ec12b02ef1d6a41f789ee3df12c4495f992f43
Michael Krebs <michikrbs@gmail.com>
2023-09-06 22:49:28 +0200

    fix: update vite config to work with webExtension

--- vite.config.ts
+++ vite.config.ts
@@ -1,66 +1,26 @@
 import { defineConfig } from "vite";
-import webExtension from "@samrum/vite-plugin-web-extension";
-import pkg from "./package.json";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+const target = process.env.TARGET || "firefox";
+
+function generateManifest() {
+  // readJsonFile instead of import() to avoid caching while development
+  const manifest = readJsonFile("manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    ...manifest,
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+  };
+}
 
 export default defineConfig({
-  build: {
-    target: "esnext",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: "[name][extname]",
-        entryFileNames: "[name].js",
-      },
-    },
-  },
   plugins: [
     webExtension({
-      manifest: {
-        name: pkg.name,
-        version: pkg.version,
-        description: pkg.description,
-        manifest_version: 2,
-        content_scripts: [
-          {
-            matches: ["<all_urls>"],
-            js: ["src/ext/content.ts"],
-          },
-        ],
-        background: {
-          scripts: ["src/ext/background.ts"],
-        },
-        options_ui: {
-          page: "src/settings/options.html",
-        },
-        icons: {
-          "32": "icon-32.png",
-          "48": "icon-48.png",
-          "64": "icon-64.png",
-          "128": "icon-128.png",
-        },
-        browser_action: {
-          default_icon: {
-            "32": "icon-32.png",
-            "48": "icon-48.png",
-            "64": "icon-64.png",
-            "128": "icon-128.png",
-          },
-          default_popup: "src/popup/index.html",
-        },
-        browser_specific_settings: {
-          gecko: {
-            id: "@mouselessV2",
-          },
-        },
-        permissions: [
-          "<all_urls>",
-          "activeTab",
-          "clipboardRead",
-          "clipboardWrite",
-          "storage",
-          "tabs",
-        ],
-      },
+      browser: target,
+      manifest: generateManifest,
+      watchFilePaths: ["package.json", "src/manifest.json"],
     }),
   ],
 });
